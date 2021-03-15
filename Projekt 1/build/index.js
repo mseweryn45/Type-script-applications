99d3f569245b705fe2ff90dba96b165b99cb4fda
mseweryn45 <mseweryn45@users.noreply.github.com>
2021-03-15 22:23:54 +0100

    Change inputs to dynamic

--- Projekt 1/build/index.js
+++ Projekt 1/build/index.js
@@ -1,42 +1,93 @@
-var ProjectApp = /** @class */ (function () {
-    function ProjectApp() {
+// class ProjectApp {
+//     dataInput1: HTMLInputElement;
+//     dataInput2: HTMLInputElement;
+//     dataInput3: HTMLInputElement;
+//     dataInput4: HTMLInputElement;
+//     sumData: HTMLInputElement;
+//     avgData: HTMLInputElement;
+//     minData: HTMLInputElement;
+//     maxData: HTMLInputElement;
+//     constructor() {
+//         this.StartApp();
+//     }
+//     StartApp() {
+//         this.getInputs();
+//         this.watchInputs();
+//     }
+//     getInputs() {
+//         this.dataInput1 = document.querySelector("#data1");
+//         this.dataInput2 = document.querySelector("#data2");
+//         this.dataInput3 = document.querySelector("#data3");
+//         this.dataInput4 = document.querySelector("#data4");
+//         this.sumData = document.querySelector("#sum");
+//         this.avgData = document.querySelector("#avg");
+//         this.minData = document.querySelector("#min");
+//         this.maxData = document.querySelector("#max");
+//     }
+//     watchInputs() {
+//         this.dataInput1.addEventListener("input", () => this.showData())
+//         this.dataInput2.addEventListener("input", () => this.showData())
+//         this.dataInput3.addEventListener("input", () => this.showData())
+//         this.dataInput4.addEventListener("input", () => this.showData())
+//     }
+//     showData() {
+//         const data1 = +this.dataInput1.value;
+//         const data2 = +this.dataInput2.value;
+//         const data3 = +this.dataInput3.value;
+//         const data4 = +this.dataInput4.value;
+//         const sum = data1 + data2 + data3 + data4;
+//         const avg = sum / 4;
+//         const min = Math.min(data1, data2, data3, data4);
+//         const max = Math.max(data1, data2, data3, data4);
+//         this.sumData.value = sum.toString();
+//         this.avgData.value = avg.toString();
+//         this.minData.value = min.toString();
+//         this.maxData.value = max.toString();
+//     }
+// }
+// let projectApp = new ProjectApp();
+var ProjectApp2 = /** @class */ (function () {
+    function ProjectApp2() {
         this.StartApp();
     }
-    ProjectApp.prototype.StartApp = function () {
-        this.getInputs();
-        this.watchInputs();
+    ProjectApp2.prototype.StartApp = function () {
+        this.GetInputs();
+        this.WatchInputs();
     };
-    ProjectApp.prototype.getInputs = function () {
-        this.dataInput1 = document.querySelector("#data1");
-        this.dataInput2 = document.querySelector("#data2");
-        this.dataInput3 = document.querySelector("#data3");
-        this.dataInput4 = document.querySelector("#data4");
-        this.sumData = document.querySelector("#sum");
-        this.avgData = document.querySelector("#avg");
-        this.minData = document.querySelector("#min");
-        this.maxData = document.querySelector("#max");
+    ProjectApp2.prototype.GetInputs = function () {
+        this.dataDynamicInput = document.querySelector("#dynamic");
+        this.box = document.querySelector(".dynamic-inputs");
     };
-    ProjectApp.prototype.watchInputs = function () {
+    ProjectApp2.prototype.WatchInputs = function () {
         var _this = this;
-        this.dataInput1.addEventListener("input", function () { return _this.showData(); });
-        this.dataInput2.addEventListener("input", function () { return _this.showData(); });
-        this.dataInput3.addEventListener("input", function () { return _this.showData(); });
-        this.dataInput4.addEventListener("input", function () { return _this.showData(); });
+        this.dataDynamicInput.addEventListener("input", function () { return _this.GetDate(); });
     };
-    ProjectApp.prototype.showData = function () {
-        var data1 = +this.dataInput1.value;
-        var data2 = +this.dataInput2.value;
-        var data3 = +this.dataInput3.value;
-        var data4 = +this.dataInput4.value;
-        var sum = data1 + data2 + data3 + data4;
-        var avg = sum / 4;
-        var min = Math.min(data1, data2, data3, data4);
-        var max = Math.max(data1, data2, data3, data4);
-        this.sumData.value = sum.toString();
-        this.avgData.value = avg.toString();
-        this.minData.value = min.toString();
-        this.maxData.value = max.toString();
+    ProjectApp2.prototype.GetDate = function () {
+        var data1 = +this.dataDynamicInput.value;
+        this.CreateElement(data1);
     };
-    return ProjectApp;
+    ProjectApp2.prototype.CreateElement = function (e) {
+        var _this = this;
+        var _loop_1 = function (i) {
+            var divElement = document.createElement("div");
+            var inputElement = document.createElement("input");
+            var buttonElement = document.createElement("button");
+            divElement.id = "data" + i;
+            buttonElement.innerText = "Remove";
+            this_1.box.appendChild(divElement);
+            divElement.appendChild(inputElement);
+            divElement.appendChild(buttonElement);
+            buttonElement.addEventListener("click", function () { return _this.RemoveElement(i); });
+        };
+        var this_1 = this;
+        for (var i = 0; i < e; i++) {
+            _loop_1(i);
+        }
+    };
+    ProjectApp2.prototype.RemoveElement = function (e) {
+        var element = document.querySelector("#data" + e);
+        element.parentNode.removeChild(element);
+    };
+    return ProjectApp2;
 }());
-var projectApp = new ProjectApp();
+var projectApp2 = new ProjectApp2();
--- Projekt 1/src/index.ts
+++ Projekt 1/src/index.ts
@@ -1,56 +1,101 @@
-class ProjectApp {
-    dataInput1: HTMLInputElement;
-    dataInput2: HTMLInputElement;
-    dataInput3: HTMLInputElement;
-    dataInput4: HTMLInputElement;
-    sumData: HTMLInputElement;
-    avgData: HTMLInputElement;
-    minData: HTMLInputElement;
-    maxData: HTMLInputElement;
+// class ProjectApp {
+//     dataInput1: HTMLInputElement;
+//     dataInput2: HTMLInputElement;
+//     dataInput3: HTMLInputElement;
+//     dataInput4: HTMLInputElement;
+//     sumData: HTMLInputElement;
+//     avgData: HTMLInputElement;
+//     minData: HTMLInputElement;
+//     maxData: HTMLInputElement;
+
+//     constructor() {
+//         this.StartApp();
+//     }
+//     StartApp() {
+//         this.getInputs();
+//         this.watchInputs();
+//     }
+
+//     getInputs() {
+//         this.dataInput1 = document.querySelector("#data1");
+//         this.dataInput2 = document.querySelector("#data2");
+//         this.dataInput3 = document.querySelector("#data3");
+//         this.dataInput4 = document.querySelector("#data4");
+//         this.sumData = document.querySelector("#sum");
+//         this.avgData = document.querySelector("#avg");
+//         this.minData = document.querySelector("#min");
+//         this.maxData = document.querySelector("#max");
+//     }
+
+//     watchInputs() {
+//         this.dataInput1.addEventListener("input", () => this.showData())
+//         this.dataInput2.addEventListener("input", () => this.showData())
+//         this.dataInput3.addEventListener("input", () => this.showData())
+//         this.dataInput4.addEventListener("input", () => this.showData())
+//     }
+
+//     showData() {
+//         const data1 = +this.dataInput1.value;
+//         const data2 = +this.dataInput2.value;
+//         const data3 = +this.dataInput3.value;
+//         const data4 = +this.dataInput4.value;
+
+//         const sum = data1 + data2 + data3 + data4;
+//         const avg = sum / 4;
+//         const min = Math.min(data1, data2, data3, data4);
+//         const max = Math.max(data1, data2, data3, data4);
+
+//         this.sumData.value = sum.toString();
+//         this.avgData.value = avg.toString();
+//         this.minData.value = min.toString();
+//         this.maxData.value = max.toString();
+
+//     }
+// }
+
+// let projectApp = new ProjectApp();
+
+class ProjectApp2 {
+    dataDynamicInput: HTMLInputElement;
+    box: HTMLElement;
 
     constructor() {
         this.StartApp();
     }
+
     StartApp() {
-        this.getInputs();
-        this.watchInputs();
+        this.GetInputs();
+        this.WatchInputs();
+    }
+    GetInputs() {
+        this.dataDynamicInput = document.querySelector("#dynamic");
+        this.box = document.querySelector(".dynamic-inputs");
+    }
+    WatchInputs() {
+        this.dataDynamicInput.addEventListener("input", () => this.GetDate())
     }
 
-    getInputs() {
-        this.dataInput1 = document.querySelector("#data1");
-        this.dataInput2 = document.querySelector("#data2");
-        this.dataInput3 = document.querySelector("#data3");
-        this.dataInput4 = document.querySelector("#data4");
-        this.sumData = document.querySelector("#sum");
-        this.avgData = document.querySelector("#avg");
-        this.minData = document.querySelector("#min");
-        this.maxData = document.querySelector("#max");
+    GetDate() {
+        const data1 = +this.dataDynamicInput.value;
+        this.CreateElement(data1);
     }
+    CreateElement(e) {
 
-    watchInputs() {
-        this.dataInput1.addEventListener("input", () => this.showData())
-        this.dataInput2.addEventListener("input", () => this.showData())
-        this.dataInput3.addEventListener("input", () => this.showData())
-        this.dataInput4.addEventListener("input", () => this.showData())
+        for (let i = 0; i < e; i++) {
+            const divElement = document.createElement("div")
+            const inputElement = document.createElement("input");
+            const buttonElement = document.createElement("button")
+            divElement.id = "data" + i;
+            buttonElement.innerText = "Remove";
+            this.box.appendChild(divElement)
+            divElement.appendChild(inputElement);
+            divElement.appendChild(buttonElement);
+            buttonElement.addEventListener("click", () => this.RemoveElement(i))
+        }
     }
-
-    showData() {
-        const data1 = +this.dataInput1.value;
-        const data2 = +this.dataInput2.value;
-        const data3 = +this.dataInput3.value;
-        const data4 = +this.dataInput4.value;
-
-        const sum = data1 + data2 + data3 + data4;
-        const avg = sum / 4;
-        const min = Math.min(data1, data2, data3, data4);
-        const max = Math.max(data1, data2, data3, data4);
-
-        this.sumData.value = sum.toString();
-        this.avgData.value = avg.toString();
-        this.minData.value = min.toString();
-        this.maxData.value = max.toString();
-
+    RemoveElement(e) {
+        const element = document.querySelector("#data" + e);
+        element.parentNode.removeChild(element);
     }
 }
-
-let projectApp = new ProjectApp();
+let projectApp2 = new ProjectApp2();
