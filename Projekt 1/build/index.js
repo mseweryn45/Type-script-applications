2f25b3d61a8bf61886b705ed0469ab20e5443a26
mseweryn45 <mseweryn45@users.noreply.github.com>
2021-03-16 23:47:49 +0100

    Added functionality of sum,avg,min,max

--- Projekt 1/build/index.js
+++ Projekt 1/build/index.js
@@ -45,7 +45,6 @@
 //         this.maxData.value = max.toString();
 //     }
 // }
-// let projectApp = new ProjectApp();
 var ProjectApp2 = /** @class */ (function () {
     function ProjectApp2() {
         this.StartApp();
@@ -53,10 +52,12 @@ var ProjectApp2 = /** @class */ (function () {
     ProjectApp2.prototype.StartApp = function () {
         this.GetInputs();
         this.WatchInputs();
+        this.CalcInputs();
     };
     ProjectApp2.prototype.GetInputs = function () {
         this.dataDynamicInput = document.querySelector("#dynamic");
         this.box = document.querySelector(".dynamic-inputs");
+        this.showbox = document.querySelector(".show-dynamic--inputs");
     };
     ProjectApp2.prototype.WatchInputs = function () {
         var _this = this;
@@ -69,25 +70,81 @@ var ProjectApp2 = /** @class */ (function () {
     ProjectApp2.prototype.CreateElement = function (e) {
         var _this = this;
         var _loop_1 = function (i) {
-            var divElement = document.createElement("div");
-            var inputElement = document.createElement("input");
-            var buttonElement = document.createElement("button");
+            var divElement = document.createElement("div"), inputElement = document.createElement("input"), buttonElement = document.createElement("button"), labelElement = document.createElement("label");
             divElement.id = "data" + i;
+            inputElement.classList.add("input-data");
+            labelElement.innerHTML = "Field:" + i;
             buttonElement.innerText = "Remove";
             this_1.box.appendChild(divElement);
             divElement.appendChild(inputElement);
             divElement.appendChild(buttonElement);
+            inputElement.addEventListener("input", function () { return _this.ShowData(inputElement.value); });
             buttonElement.addEventListener("click", function () { return _this.RemoveElement(i); });
         };
         var this_1 = this;
         for (var i = 0; i < e; i++) {
             _loop_1(i);
         }
+        console.log(e);
+        if (e == 0) {
+            var elements = document.querySelectorAll(".dynamic-inputs > div");
+            elements.forEach(function (element) {
+                element.parentNode.removeChild(element);
+            });
+        }
+    };
+    ProjectApp2.prototype.CalcInputs = function () {
+        var sumInput = document.createElement("input"), sumLabel = document.createElement("label"), avgInput = document.createElement("input"), avgLabel = document.createElement("label"), minInput = document.createElement("input"), minLabel = document.createElement("label"), maxInput = document.createElement("input"), maxLabel = document.createElement("label");
+        sumLabel.innerHTML = "Sum:";
+        avgLabel.innerHTML = "Avg:";
+        minLabel.innerHTML = "Min:";
+        maxLabel.innerHTML = "Max:";
+        sumInput.id = "sum";
+        avgInput.id = "avg";
+        minInput.id = "min";
+        maxInput.id = "max";
+        this.showbox.appendChild(sumLabel);
+        this.showbox.appendChild(sumInput);
+        this.showbox.appendChild(avgLabel);
+        this.showbox.appendChild(avgInput);
+        this.showbox.appendChild(minLabel);
+        this.showbox.appendChild(minInput);
+        this.showbox.appendChild(maxLabel);
+        this.showbox.appendChild(maxInput);
     };
     ProjectApp2.prototype.RemoveElement = function (e) {
         var element = document.querySelector("#data" + e);
         element.parentNode.removeChild(element);
     };
+    ProjectApp2.prototype.ShowData = function (e) {
+        var inputList = document.querySelectorAll(".input-data");
+        var numberList = [];
+        inputList.forEach(function (element) {
+            numberList.push(+element.value);
+        });
+        var min = Math.min.apply(Math, numberList);
+        var max = Math.max.apply(Math, numberList);
+        var avg = numberList.reduce(function (a, b) { return a + b; }, 0) / numberList.length;
+        var sum = numberList.reduce(function (a, b) { return a + b; }, 0);
+        var text = "Only numbers";
+        this.sumData = document.querySelector("#sum");
+        this.avgData = document.querySelector("#avg");
+        this.minData = document.querySelector("#min");
+        this.maxData = document.querySelector("#max");
+        if (isNaN(e)) {
+            this.sumData.value = text;
+            this.avgData.value = text;
+            this.minData.value = text;
+            this.maxData.value = text;
+        }
+        else {
+            this.sumData.value = sum.toString();
+            this.avgData.value = avg.toString();
+            this.minData.value = min.toString();
+            this.maxData.value = max.toString();
+        }
+    };
     return ProjectApp2;
 }());
+// let projectApp = new ProjectApp();
 var projectApp2 = new ProjectApp2();
--- Projekt 1/src/index.ts
+++ Projekt 1/src/index.ts
@@ -53,11 +53,17 @@
 //     }
 // }
 
-// let projectApp = new ProjectApp();
 
 class ProjectApp2 {
     dataDynamicInput: HTMLInputElement;
-    box: HTMLElement;
+    box: HTMLDivElement;
+    showbox: HTMLDivElement;
+    sumData: HTMLInputElement;
+    avgData: HTMLInputElement;
+    minData: HTMLInputElement;
+    maxData: HTMLInputElement;
+    helpData: number;
+    list: Array<number>;
 
     constructor() {
         this.StartApp();
@@ -66,10 +72,13 @@ class ProjectApp2 {
     StartApp() {
         this.GetInputs();
         this.WatchInputs();
+        this.CalcInputs();
     }
     GetInputs() {
         this.dataDynamicInput = document.querySelector("#dynamic");
         this.box = document.querySelector(".dynamic-inputs");
+        this.showbox = document.querySelector(".show-dynamic--inputs")
+
     }
     WatchInputs() {
         this.dataDynamicInput.addEventListener("input", () => this.GetDate())
@@ -82,20 +91,95 @@ class ProjectApp2 {
     CreateElement(e) {
 
         for (let i = 0; i < e; i++) {
-            const divElement = document.createElement("div")
-            const inputElement = document.createElement("input");
-            const buttonElement = document.createElement("button")
+            const divElement = document.createElement("div"),
+                inputElement = document.createElement("input"),
+                buttonElement = document.createElement("button"),
+                labelElement = document.createElement("label");
             divElement.id = "data" + i;
+            inputElement.classList.add("input-data");
+            labelElement.innerHTML = "Field:" + i;
             buttonElement.innerText = "Remove";
             this.box.appendChild(divElement)
             divElement.appendChild(inputElement);
             divElement.appendChild(buttonElement);
+            inputElement.addEventListener("input", () => this.ShowData(inputElement.value));
             buttonElement.addEventListener("click", () => this.RemoveElement(i))
         }
+        if (e == 0) {
+            let elements = document.querySelectorAll(".dynamic-inputs > div");
+            elements.forEach(element => {
+                element.parentNode.removeChild(element);
+            });
+
+        }
     }
+    CalcInputs() {
+        const sumInput = document.createElement("input"),
+            sumLabel = document.createElement("label"),
+            avgInput = document.createElement("input"),
+            avgLabel = document.createElement("label"),
+            minInput = document.createElement("input"),
+            minLabel = document.createElement("label"),
+            maxInput = document.createElement("input"),
+            maxLabel = document.createElement("label");
+
+        sumLabel.innerHTML = "Sum:";
+        avgLabel.innerHTML = "Avg:";
+        minLabel.innerHTML = "Min:";
+        maxLabel.innerHTML = "Max:";
+        sumInput.id = "sum";
+        avgInput.id = "avg";
+        minInput.id = "min";
+        maxInput.id = "max";
+
+        this.showbox.appendChild(sumLabel);
+        this.showbox.appendChild(sumInput);
+        this.showbox.appendChild(avgLabel);
+        this.showbox.appendChild(avgInput);
+        this.showbox.appendChild(minLabel);
+        this.showbox.appendChild(minInput);
+        this.showbox.appendChild(maxLabel);
+        this.showbox.appendChild(maxInput);
+    }
+
+
     RemoveElement(e) {
         const element = document.querySelector("#data" + e);
         element.parentNode.removeChild(element);
     }
+
+    ShowData(e) {
+        let inputList: NodeListOf<HTMLInputElement> = document.querySelectorAll(".input-data");
+        let numberList: Array<number> = [];
+        inputList.forEach(element => {
+            numberList.push(+element.value)
+        });
+        const min = Math.min(...numberList)
+        const max = Math.max(...numberList);
+        const avg = numberList.reduce((a, b) => a + b, 0) / numberList.length;
+        const sum = numberList.reduce((a, b) => a + b, 0);
+        const text = "Only numbers";
+
+        this.sumData = document.querySelector("#sum");
+        this.avgData = document.querySelector("#avg");
+        this.minData = document.querySelector("#min");
+        this.maxData = document.querySelector("#max");
+
+
+        if (isNaN(e)) {
+            this.sumData.value = text;
+            this.avgData.value = text;
+            this.minData.value = text;
+            this.maxData.value = text;
+        } else {
+            this.sumData.value = sum.toString();
+            this.avgData.value = avg.toString();
+            this.minData.value = min.toString();
+            this.maxData.value = max.toString();
+        }
+
+    }
 }
+
+// let projectApp = new ProjectApp();
 let projectApp2 = new ProjectApp2();
