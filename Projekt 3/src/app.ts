7d11f6f684f475b6842115a4c7b3391ae83a0bd4
mseweryn45 <63972438+mseweryn45@users.noreply.github.com>
2021-04-11 14:25:14 +0200

    weater fuctionality added

--- Projekt 3/src/app.ts
+++ Projekt 3/src/app.ts
@@ -1,13 +1,46 @@
 export class App {
-    login = 'asd'
+    apiKey = 'a4cb374648481f3247013e5f9ead66e2'
+    cityInput: HTMLInputElement;
+    cityName: string;
+    data: string;
     constructor() {
-        // const b = new Form()
+        this.getInput();
     }
-    getThis(e: any) {
-        console.log(this, e)
+    
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather(city);
+        this.saveData(weather.name);
     }
-    aa() {
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        
+        return weatherData;
+    }
 
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
+
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            const parseData = JSON.parse(data);
+            return parseData
+        } else {
+            return {};
+        }
+    }
+
+    getInput(){
+        this.cityInput = document.querySelector("#cityInput");
+        this.cityInput.addEventListener("input", () => this.showData())
+    }
+
+    showData(){
+        const data = this.cityInput.value;
+        this.getCityInfo(data);
     }
 }
-export const AAA = 10;
--- Projekt 3/src/index.ts
+++ Projekt 3/src/index.ts
@@ -1,8 +1,4 @@
 import { App } from './app';
 import './main.scss';
 
-const hello = "yep, it's workin still sick";
-document.body.innerHTML = hello;
 const app = new App();
-app.getThis('');
-document.addEventListener('click', (ev)=>{app.getThis(ev)})
