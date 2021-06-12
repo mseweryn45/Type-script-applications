369ed55c65a5c0fa3625d1c00ef2062249da4ef7
mseweryn45 <63972438+mseweryn45@users.noreply.github.com>
2021-06-12 10:33:26 +0200

    Change file structure

--- Projekt 4/src/app.ts
+++ Projekt 4/src/app.ts
@@ -1,12 +1,13 @@
+import { Note } from './note'
+const note = new Note();
+
 export class App {
-    apiKey = 'a4cb374648481f3247013e5f9ead66e2'
     btnAdd: HTMLButtonElement;
     titleInput: HTMLInputElement;
     descriptionInput: HTMLInputElement;
-    cityName: string;
     data: string;
     weaterBox: HTMLDivElement;
-    data1: string;
+
     constructor() {
         this.getInput();
         this.getData();
@@ -17,10 +18,8 @@ export class App {
         let json = JSON.stringify({ title, description });
         localStorage.setItem('Note' + keys, json);
         const parseData = JSON.parse(json);
-        this.createBox(parseData, 'Note' + keys)
+        note.createBox(parseData, 'Note' + keys)
     }
-
-
 
     getData() {
         let keys = Object.keys(localStorage).length - 1;
@@ -29,9 +28,10 @@ export class App {
                 const data = localStorage.getItem('Note' + i);
                 const localStorageName = 'Note' + i;
                 const parseData = JSON.parse(data);
-                this.createBox(parseData, localStorageName)
+                note.createBox(parseData, localStorageName)
             }
         } else {
+            console.log("tal")
             return {};
         }
     }
@@ -49,37 +49,6 @@ export class App {
         const title = this.titleInput.value;
         const description = this.descriptionInput.value;
         this.saveData(title, description);
-    }
-
-
-    createBox(data: any, localStorageName: string) {
-        const title = data.title;
-        const description = data.description;
-        const weaterBox = document.querySelector(".city");
-        const divElement = document.createElement("div");
-        const buttonElement = document.createElement("button")
-        buttonElement.addEventListener("click", function () {
-            divElement.parentElement.removeChild(divElement)
-            localStorage.removeItem(localStorageName);
-        })
-        buttonElement.innerText = "Remove";
-        divElement.classList.add("city_card");
-        this.createCountry(divElement, title)
-        this.createWeather(divElement, description)
-        weaterBox.appendChild(divElement)
-        divElement.appendChild(buttonElement)
-    }
-    createCountry(elem: HTMLDivElement, title: string) {
-        const spanElement = document.createElement("span");
-        spanElement.classList.add("city__box--top-country");
-        spanElement.innerText = title;
-        elem.appendChild(spanElement)
-    }
-    createWeather(elem: HTMLDivElement, weatherType: string) {
-        const spanElement = document.createElement("span");
-        spanElement.classList.add("city__box--top-weater");
-        spanElement.innerHTML = weatherType
-        elem.appendChild(spanElement)
     }
 
 }
